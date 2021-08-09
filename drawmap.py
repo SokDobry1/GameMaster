014bd5005dc9cb49e9f52229c02d71cf9f35ad5d
SokDobry1 <officialaccalf@gmail.com>
2021-08-09 20:07:50 +0300

    первая полностью готовая версия

--- drawmap.py
+++ drawmap.py
@@ -1,11 +1,12 @@
 from PIL import ImageDraw, Image, ImageFont
 from random import randint
 from io import BytesIO
+from db import get_player_by_id
 
 from discord.ext.commands.converter import TextChannelConverter
 
-def centered_table(w, h, txt):
-    txtbox = Image.new('RGB', (w, h), 'white')
+def centered_table(w, h, txt, color):
+    txtbox = Image.new('RGB', (w, h), color)
     drawtxt = ImageDraw.Draw(txtbox)
     drawtxt.rectangle((0,0,w-1,h-1), outline=1)
     font = ImageFont.truetype('arial.ttf', 18)
@@ -18,7 +19,7 @@ def load_map(players_data, env_data):
     map = [[-1 for i in range(sz - 1)] for i in range(18)]
     for x in players_data:
         pos = x['pos'].split(':')
-        map[int(pos[0]) - 1][int(pos[1]) - 1] = 1
+        map[int(pos[0]) - 1][int(pos[1]) - 1] = x['id']
     rectSize = 40
     w = sz * rectSize + 1
     h = sz * rectSize + 1
@@ -26,47 +27,50 @@ def load_map(players_data, env_data):
     draw = ImageDraw.Draw(img)
     fpx, fpy = 0, rectSize
     font = ImageFont.truetype('arial.ttf', 20)
-    lit = 'A'
+    lit = 1
     draw.rectangle([0, 0, rectSize, rectSize], outline=1)
     for i in range(sz):
         draw.rectangle([fpx, fpy, fpx+rectSize, fpy+rectSize], outline=1)
-        draw.text((fpx + rectSize // 4, fpy + rectSize // 4), lit, font=font, fill='black')
+        draw.text((fpx + rectSize // 4, fpy + rectSize // 4), str(lit), font=font, fill='black')
         fpy += rectSize
-        lit = chr(ord(lit) + 1)
-    lit = 1
+        lit += 1
+    lit = 'A'
     fpx, fpy = rectSize, 0
     for i in range(sz):
         draw.rectangle([fpx, fpy, fpx+rectSize, fpy+rectSize], outline=1)
-        draw.text((fpx + rectSize // 4, fpy + rectSize // 4), str(lit), font=font, fill='black')
+        draw.text((fpx + rectSize // 4, fpy + rectSize // 4), lit, font=font, fill='black')
         fpx += rectSize
-        lit += 1
+        lit = chr(ord(lit) + 1)
     fpx, fpy = rectSize, rectSize
+    colors = {}
+    font = ImageFont.truetype('arial.ttf', 16)
     for i in range(sz - 1):
         for j in range(sz - 1):
             if (map[i][j] == -1):
                 draw.rectangle([fpx, fpy, fpx+rectSize, fpy+rectSize], outline=1)
             else:
-                r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+                r, g, b = (int(str(map[i][j])[::-1]) * 1244 + 144) ** 3 % 255, ((map[i][j] * 5352 + 42311) * 14221) % 255, ((map[i][j] * 1244 + 144) ** 3) % 255
                 draw.rectangle([fpx, fpy, fpx+rectSize, fpy+rectSize], fill=(r, g, b), outline=1)
-                draw.text((fpx + rectSize // 4, fpy + rectSize // 4), 'P', font=font)
+                pl = get_player_by_id(str(map[i][j]))
+                draw.text((fpx + 2, fpy + 6), pl['name'][0:3], font=font, stroke_width=1, stroke_fill='black')
+                colors[map[i][j]] = (r, g, b)
             fpx += rectSize
         fpx = rectSize
         fpy += rectSize
     widthes = [30*14, 30*4-30, 30*2-4, 30*5+6, 30*5-2]
-    table = Image.new('RGB', (1000, (sz-1) * 40 + 1), 'white')
-    tableHeight = 18 * 40 + 1 // 3
+    table = Image.new('RGB', (1000, 32 * (len(players_data) + 1) - 2), 'white')
     fpx, fpy = 0, 0
-    table.paste(centered_table(widthes[0], 32, 'Игрок'), (fpx, fpy))
+    table.paste(centered_table(widthes[0], 32, 'Игрок', (255, 255, 255)), (fpx, fpy))
     fpx += 30 * 14 - 1
-    table.paste(centered_table(widthes[1], 32, 'Здоровье'), (fpx, fpy))
+    table.paste(centered_table(widthes[1], 32, 'Здоровье', (255, 255, 255)), (fpx, fpy))
     fpx += 30*4-30 - 1
-    table.paste(centered_table(widthes[2], 32, 'ОД'), (fpx, fpy))
+    table.paste(centered_table(widthes[2], 32, 'ОД', (255, 255, 255)), (fpx, fpy))
     fpx += 30 * 2 - 4 - 1
-    table.paste(centered_table(widthes[3], 32, 'Получил помощи'), (fpx, fpy))
+    table.paste(centered_table(widthes[3], 32, 'Получил помощи', (255, 255, 255)), (fpx, fpy))
     fpx += 30 * 5 + 6 - 1
-    table.paste(centered_table(widthes[4], 32, 'Оказал помощи'), (fpx, fpy))
+    table.paste(centered_table(widthes[4], 32, 'Оказал помощи', (255, 255, 255)), (fpx, fpy))
     fpx += 30 * 5 - 2 - 1
-    table.paste(centered_table(1000 - fpx, 32, 'Нанес урона'), (fpx, fpy))
+    table.paste(centered_table(1000 - fpx, 32, 'Нанес урона', (255, 255, 255)), (fpx, fpy))
     widthes.append(1000 - fpx)
     fpy += 32
     fpx = 0
@@ -74,12 +78,12 @@ def load_map(players_data, env_data):
         i = 0
         for x in p:
             if x not in ["id", "pos"]:
-                table.paste(centered_table(widthes[i], 32, str(p[x])), (fpx, fpy))
-                fpx += widthes[i]
+                table.paste(centered_table(widthes[i], 32, str(p[x]), colors[p['id']]), (fpx, fpy))
+                fpx += widthes[i] - 1
                 i += 1
         fpx, i = 0, 0
-        fpy += 32
-    ret = Image.new('RGB', (img.width + table.width, max(img.height, table.height)), (0,0,0))
+        fpy += 32 - 1
+    ret = Image.new('RGB', (img.width + table.width, max(img.height, table.height)), 'white')
     ret.paste(img, (0, 0))
     ret.paste(table, (img.width, 0))
 
